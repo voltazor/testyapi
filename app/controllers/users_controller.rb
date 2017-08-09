9dc0cf52a76c8efcb4a48e14bfcf44ef086bb432
Dmitriy Dovbnya <dmitriy.dovbnya@yalantis.com>
2017-08-09 17:54:26 +0300

    users put, post, delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,8 @@ class UsersController < ApplicationController
       if @user.save
         @user.token = Digest::SHA2.hexdigest(@user.id.to_s + @user.email + @user.password)
         @user.save
-        redirect_to @user
+        # redirect_to @user
+        render json: UserSerializer.new(@user,  true)
       else
         render json: ErrorSerializer.new('failed'), status: 500
       end
@@ -22,24 +23,78 @@ class UsersController < ApplicationController
   end
 
   def show
-    render json: UserSerializer.new(User.where(id: params[:id]).first)
+    user = get_user(params[:id])
+    if user.nil?
+      render json: ErrorSerializer.new('User not found'), status: 404
+    else
+      render json: UserSerializer.new(get_user(params[:id]),  false)
+    end
+  end
+
+  def update
+    header = request.headers['Authorization'].to_s
+    user = get_user(params[:id])
+    if user.nil?
+      render json: ErrorSerializer.new('User not found'), status: 404
+    else
+      if header.eql?(user.token)
+        unless params[:name].nil?
+          user.name = params[:name]
+        end
+        unless params[:email].nil?
+          user.email = params[:email]
+        end
+        unless params[:password].nil?
+          user.password = params[:password]
+        end
+        user.save
+        render plain: 'Success', status: 200
+      else
+        render json: ErrorSerializer.new('Unauthorized'), status: 401
+      end
+    end
+  end
+
+  def destroy
+    header = request.headers['Authorization'].to_s
+    user = get_user(params[:id])
+    if user.nil?
+      render json: ErrorSerializer.new('User not found'), status: 404
+    else
+      if header.eql?(user.token)
+        User.delete(params[:id])
+        render plain: 'Success', status: 200
+      else
+        render json: ErrorSerializer.new('Unauthorized'), status: 401
+      end
+    end
+  end
+
+  def check_token(user_id, token)
+    get_user(user_id).token.eql?(token)
+  end
+
+  def get_user(user_id)
+    User.where(id: user_id).first
   end
 
   def format_users(users)
     formatted_users = []
     users.each do |user|
-      formatted_users << UserSerializer.new(user)
+      formatted_users << UserSerializer.new(user, false)
     end
     formatted_users
   end
 
   class UserSerializer
 
-    def initialize(user)
+    def initialize(user, show_token)
       @id = user.id
       @email = user.email
       @name = user.name
-      @token = user.token
+      # if show_token
+        @token = user.token
+      # end
     end
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   delete 'api' => 'home#delete'
 
-  resources :users
+  resources :users, only: [:index, :show, :create, :update, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
